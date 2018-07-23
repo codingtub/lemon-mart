26981a45f297b31af6d444da27190ee9967c9a4f
Michael Rieger <mike.rieger10@gmail.com>
2018-07-23 16:32:21 +0200

    Added additional modules and icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './/material.module';
+import { ManagerModule } from './manager/manager.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { PosModule } from './pos/pos.module';
+import { UserModule } from './user/user.module';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
+  declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ManagerModule,
+    InventoryModule,
+    PosModule,
+    UserModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
