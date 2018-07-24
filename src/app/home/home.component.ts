e6cb65c04bf823eefcdcafa45d7c42f3559bdc60
Michael Rieger <mike.rieger10@gmail.com>
2018-07-24 11:24:24 +0200

    Added components to manager module, implemented lazy loading for modules

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,19 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `
-    <div fxLayout="column" fxLayoutAlign="center center">
-      <span class="mat-display-2">Hello, Lemonite!</span>
-      <button mat-raised-button color="primary">Login</button>
-    </div>
-  `,
-  styles: [
-    `
-      div[fxLayout] {
-        margin-top: 32px;
-      }
-    `,
-  ],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   constructor() {}
