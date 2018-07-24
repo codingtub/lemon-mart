cd51d03f730c0084ba57238027c2dfa8939e6e3a
Michael Rieger <mike.rieger10@gmail.com>
2018-07-24 15:58:19 +0200

    Updated build scripts and test configuration

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,7 +25,28 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromiumHeadless', 'ChromiumNoSandbox'],
+    customLaunchers: {
+      ChromiumHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          // Without a remote debugging port, Google Chrome exits immediately.
+          '--remote-debugging-port=9222',
+        ],
+        debug: true,
+      },
+      ChromiumNoSandbox: {
+        base: 'ChromiumHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-translate',
+          '--disable-extensions',
+        ],
+        debug: true,
+      },
+    },
     singleRun: false
   });
 };
